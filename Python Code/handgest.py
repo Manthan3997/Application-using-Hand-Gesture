eacc564ca454f23c2eb309aba66aecd5a756eceb
Manthan3997 <105127849+Manthan3997@users.noreply.github.com>
2022-05-08 01:26:32 +0530

    Add files via upload

---
+++ Python Code/handgest.py
@@ -0,0 +1,33 @@
+import serial                                      # add Serial library for serial communication
+import pyautogui                                   # add pyautogui library for programmatically controlling the mouse and keyboard.
+import time 
+
+Arduino_Serial = serial.Serial('com6',9600) # Initialize serial and Create Serial port object called Arduino_Serial
+time.sleep(2)#wait for 2 seconds for the communication to get established
+
+ 
+while 1:
+    incoming_data = str (Arduino_Serial.readline()) # read the serial data and print it as line
+    print (incoming_data)                            # print the incoming Serial data
+     
+
+    if 'next' in incoming_data:                    # if incoming data is 'next'
+        pyautogui.hotkey('ctrl','shift','tab')      # perform "ctrl+shift+tab" operation which moves to the next tab
+        
+    if 'previous' in incoming_data:                # if incoming data is 'previous'
+        pyautogui.hotkey('ctrl', 'pgup')            # perform "ctrl+pgup" operation which moves to the previous tab
+
+    if 'down' in incoming_data:                    # if incoming data is 'down'
+        pyautogui.press('down')                   # performs "down arrow" operation which scrolls down the page
+        pyautogui.scroll(-50) 
+         
+    if 'up' in incoming_data:                      # if incoming data is 'up'
+        pyautogui.press('up')                      # performs "up arrow" operation which scrolls up the page
+        pyautogui.scroll(50)
+        
+    if 'change' in incoming_data:                  # if incoming data is 'change'      
+        pyautogui.keyDown('alt')                   # performs "alt+tab" operation which switches the tab
+        pyautogui.press('tab')
+        pyautogui.keyUp('alt')
+        
+    incoming_data = "";
